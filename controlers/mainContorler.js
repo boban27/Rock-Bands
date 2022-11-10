c664079abf40b659d9141251e004b6bc9954a0e3
boban27 <103284878+boban27@users.noreply.github.com>
2022-11-10 10:51:38 +0100

    Add files via upload

---
+++ controlers/mainContorler.js
@@ -0,0 +1,20 @@
+angular.module('mainContoller',[])
+.controller('mainCtrl',function($scope,db){
+    $scope.bands = [];
+$scope.activeBand - {};
+
+db.getData()
+.then(function (result) {
+    $scope.bands = result.data;
+    
+
+},function (error) {
+    console.log(error)
+
+})
+$scope.display = function (band) {
+    $scope.activeBand = band;
+    
+}
+
+})
